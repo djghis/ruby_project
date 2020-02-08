3fd64086f3727e48435bb850a05cc5cc240608d1
roosap <roosa.paivansalo@gmail.com>
2020-02-08 11:29:57 +0000

    Add database tables

---
+++ db/money_tracker.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS budgets;
+
+CREATE TABLE budgets (
+  id SERIAL PRIMARY KEY,
+  amount FLOAT
+);
+
+CREATE TABLE merchants (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE transactions (
+  id SERIAL PRIMARY KEY,
+  merchant_id INT REFERENCES merchants('id'),
+  tag_id INT REFERENCES tags('id')
+);
