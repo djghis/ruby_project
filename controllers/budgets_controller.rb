97b78fd765124320f7b490d55d4d35f4a58977b7
roosap <roosa.paivansalo@gmail.com>
2020-02-10 19:24:04 +0000

    Add error messgae for negative budget

--- controllers/budgets_controller.rb
+++ controllers/budgets_controller.rb
@@ -6,12 +6,19 @@ require_relative('../models/budget')
 also_reload('../models/*')
 
 get '/budget/update' do
+  if params[:error] == "true"
+    @error = "Sorry, your budget has to be more than 0!"
+  end
   erb(:'budget/update')
 end
 
 post '/budget' do
  Budget.delete_all()
  budget = Budget.new(params)
- budget.save
- redirect to '/'
+  if budget.amount.to_i < 0
+    redirect to '/budget/update?error=true'
+  else
+    budget.save
+    redirect to '/'
+  end
 end
