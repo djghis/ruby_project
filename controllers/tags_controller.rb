7924a9f49628d7e0553c3a7fa68cd94bc41e9ca0
roosap <roosa.paivansalo@gmail.com>
2020-02-10 14:41:40 +0000

    Add new tag routes and views

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -10,3 +10,13 @@ get '/tags' do
   @tags = Tag.all()
   erb(:'tags/index')
 end
+
+get '/tags/new' do
+  erb(:'tags/new')
+end
+
+post '/tags' do
+ @tag = Tag.new(params)
+ @tag.save
+ redirect to '/tags'
+end
